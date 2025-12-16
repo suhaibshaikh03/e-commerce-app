3e74fab465dcba585a839591e8b0b4236b74497c
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2025-12-16 14:17:49 +0500

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Link from  "next/link"
 const Footer = () => {
 return (
-    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start bg-gray-800 p-8 rounder-lg md:justify-between md:gap-0  ">
+    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start bg-gray-800 p-8 rounded-lg md:justify-between md:gap-0  ">
         <div className="flex flex-col gap-4 items-center md:items-start ">
         <Link href="/" className="flex items-center">
             <Image

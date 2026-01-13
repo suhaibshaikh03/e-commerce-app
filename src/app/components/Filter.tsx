29ff5af2b0237b859a9ccb7c951ccc2ac4b86af9
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2026-01-13 18:51:25 +0500

    Update Filter.tsx

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -6,9 +6,21 @@ const Filter = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
     const pathname = usePathname();
-    return <div className="">
+
+    const handleFilter = (value: string) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("sort", value);
+        router.push(`${pathname}?${params.toString()}`,{scroll: false});
+    };
+
+
+
+
+
+    return (
+    <div className="flex items-center justify-end gap-2 text-sm text-gray-500 my-6">
         <span>Sort by:</span>
-        <select name="sort" id="sort">
+        <select name="sort" id="sort" className="ring-1 ring-gray-200 shadow-md p-1 rounded-sm">
             <option value="newest">Newest</option>
             <option value="oldest">Oldest</option>
             <option value="asc">Price: Low to High</option>
@@ -16,6 +28,7 @@ const Filter = () => {
         </select>
 
     </div>
+    );
 }
 
 export default Filter;

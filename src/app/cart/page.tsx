1ff6eda5b894b6f8352bce2a8a8800f384d45c81
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2026-01-14 14:36:10 +0500

    update /cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -109,18 +109,36 @@ const CartPage = () => {
         <div className="w-full lg:w-5/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
             <h2 className="font-semibold">Cart Details</h2>
             <div className="flex flex-col gap-4">
-                <div className="flex justify-between">
-                    <p>Subtotal</p>
-                    <p>${cartItems.reduce(
+                <div className="flex justify-between text-sm">
+                    <p className="text-gray-500">Subtotal</p>
+                    <p className=" font-medium">${cartItems.reduce(
                         (acc, item) => acc + item.price * item.quantity,
                         0
-                    )}</p>
+                    ).toFixed(2)}</p>
+                </div>
+                 <div className="flex justify-between text-sm">
+                    <p className="text-gray-500">Discount(10%)</p>
+                    <p className=" font-medium">$10</p>
+                </div>
+                 <div className="flex justify-between text-sm">
+                    <p className="text-gray-500">Shipping Fee</p>
+                    <p className=" font-medium">$10</p>
+                </div>
+                <hr className="border-gray=200"/>
+                 <div className="flex justify-between">
+                    <p className="text-gray-800 font-semibold">Total</p>
+                    <p className=" font-medium">${cartItems.reduce(
+                        (acc, item) => acc + item.price * item.quantity,
+                        0
+                    ).toFixed(2)}</p>
                 </div>
             </div>
-            <button className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2">
+           {activeStep===1 && 
+           <button onClick={()=>router.push("/cart?step=2",{scroll:false})} className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2">
                 Continue
                 <ArrowRight className="w-3 h-3"/>
                 </button>
+                }
         </div>
         </div>
         </div>

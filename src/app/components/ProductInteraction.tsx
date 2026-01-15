74752d8c19c42d51fcb6e1b592bb0f3ee4270b0f
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2026-01-15 15:56:38 +0500

    update product interaction

--- src/app/components/ProductInteraction.tsx
+++ src/app/components/ProductInteraction.tsx
@@ -1,18 +1,34 @@
 "use client"
 import { ProductType } from "@/types";
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, ShoppingCart } from "lucide-react";
 import { useState } from "react";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import useCartStore from "@/stores/CartStore";
 
 const ProductInteraction = ({ product, selectedSize, selectedColor }: { product: ProductType; selectedSize: string; selectedColor: string; }) => {
     const router = useRouter()
     const pathname = usePathname()
     const searchParams = useSearchParams()
     const [quantity, setQuantity] = useState(1)
+
+    const {addToCart} = useCartStore()
+
     const handleTypeChange = (type: string, value: string) => {
         const params = new URLSearchParams(searchParams.toString())
         params.set(type, value)
         router.push(`${pathname}?${params.toString()}`, { scroll: false })
+    }
+    const handleQuantityChange = (type: "increment" | "decrement") => {
+        if(type==="increment"){
+            setQuantity((prev)=>prev+1)
+        }else{
+            if(quantity>1){
+                setQuantity((prev)=>prev-1);
+            }
+        }
+    }
+    const handleAddtoCart = ()=>{
+
     }
     return (
         <div className="flex flex-col gap-4 mt-4">
@@ -53,16 +69,26 @@ const ProductInteraction = ({ product, selectedSize, selectedColor }: { product:
             <div className="flex flex-col gap-2 text-sm">
                 <span className="text-gray-500">Quantity</span>
                 <div className="flex items-center gap-2">
-                    <button className="cursor-pointer border-1 border-gray-300 p-1" onClick={()=>}>
+                    <button className="cursor-pointer border-1 border-gray-300 p-1" onClick={()=>handleQuantityChange("decrement")}>
                         <Minus className="w-4 h-4"/>
                     </button>
                     <span>{quantity}</span>
-                    <button className="cursor-pointer border-1 border-gray-300 p-1">
+
+                    <button className="cursor-pointer border-1 border-gray-300 p-1" onClick={()=>handleQuantityChange("increment")}>
                         <Plus className="w-4 h-4"/>
                     </button>
                 </div>
 
             </div>
+            {/* BUTTONS */}
+            <button onClick={handleAddtoCart} className="bg-gray-800 text-white px-4 py-2 rounded-md shadow-lg flex items-center justify-center gap-2 cursor-pointer text-sm font-medium">
+                <Plus className="w-4 h-4"/>
+                Add to Cart
+            </button>
+            <button className="ring-1 ring-gray-400 shadow-lg text-gray-800 px-4 py-2 rounded-md flex items-center justify-center cursor-pointer gap-2 text-sm font-medium">
+                <ShoppingCart className="w-4 h-4"/>
+                Buy this Item
+            </button>
 
         </div>
     );

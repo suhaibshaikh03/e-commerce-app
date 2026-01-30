96f5a72a465184c6933b1dbc7f9e3247a01cf820
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2026-01-31 01:40:59 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    unoptimized: true, // Since all images are local, we can disable optimization
+  },
+};
 
 export default nextConfig;

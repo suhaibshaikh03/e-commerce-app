c902e39e1d04fa057d286daa52eab7b875f431b7
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2026-01-31 14:15:18 +0500

    Revert "update"
    This reverts commit 279376425b48d38aa83be42f5362012be188e528.

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
 const nextConfig: NextConfig = {};
-=======
-const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: false, // Disable for static export
-  },
-  images: {
-    unoptimized: true, // Since all images are local, we can disable optimization
-  },
-  // Use static export for simpler deployment
-  output: 'export',
-  trailingSlash: true,
-};
->>>>>>> parent of e15edda (update)
 
 export default nextConfig;

8d23c0c903c1e4cd4c672a73bde46c9ec1b69c55
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2026-01-31 14:08:25 +0500

    Revert "update"
    This reverts commit 96f5a72a465184c6933b1dbc7f9e3247a01cf820.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  images: {
-    unoptimized: true, // Since all images are local, we can disable optimization
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
